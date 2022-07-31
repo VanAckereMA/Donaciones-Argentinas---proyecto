196b113e35567a325511356e08a9462a57f86637
Fernando Flores <fernando.floresmancilla@hotmail.com>
2022-07-31 20:23:01 -0300

    Create new file, nerby-places, and its style and script

---
+++ JS/map.js
@@ -0,0 +1,20 @@
+var map = L.map('map').setView([-34.61315, -58.37723], 13);
+
+/*L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.streetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);*/
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© DonArg'
+    }).addTo(map);
+
+googleStreets = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+googleStreets.addTo(map);
+document.getElementById('select-location').addEventListener('change', function(e){
+    let coords = e.target.value.split(",");
+    map.flyTo(coords, 18);
+})
+
