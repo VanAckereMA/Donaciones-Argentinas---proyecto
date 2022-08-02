236643dd58a5bb4c2f0acb962d8e6c66a1fe0351
Fernando Flores <fernando.floresmancilla@hotmail.com>
2022-08-01 23:12:45 -0300

    change nearby-places's design

--- JS/map.js
+++ JS/map.js
@@ -4,17 +4,20 @@ var map = L.map('map').setView([-34.61315, -58.37723], 13);
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.streetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);*/
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© DonArg'
-    }).addTo(map);
+
 
 googleStreets = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}',{
     maxZoom: 20,
+    attribution: '© DonArg',
     subdomains:['mt0','mt1','mt2','mt3']
 });
+
 googleStreets.addTo(map);
+
+
 document.getElementById('select-location').addEventListener('change', function(e){
     let coords = e.target.value.split(",");
     map.flyTo(coords, 18);
-})
-
+    L.marker(coords).addTo(map);
+});
+   
